edb77c052275de6cc9aa37a049177451ceed4a8a
saba farzanegan <saba.far.99@gmail.com>
2025-09-28 16:06:26 +0330

    add modals

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,13 @@
  * @copyright codewithsadee 2023
  */
 
-'use strict';
+"use strict";
+
+const modal_btn = document.querySelector(".main .fab");
+
+const modal = document.querySelector(".modal");
+modal_btn.addEventListener("click", () => {
+  modal.showModal();
+
+  console.log("modal trigger");
+});
